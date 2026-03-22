7a04ac6ec06a08e915895676f3df8aff4496bce4
JosehidalgoVallejos <jhidalgovallejos@gmail.com>
2026-03-21 21:06:47 -0600

    hola

--- DiagnoSys.sql
+++ DiagnoSys.sql
@@ -952,16 +952,117 @@ go
 -------------------------------------------------------------------
 --Tabla Anexos
 --Procedimiento almacenado
+Create Table Anexo (
+	IdAnexo int Primary Key,
+	NombreAnexo varchar(50)
+)
+GO
 
 --Adicion de datos
+Create PROCEDURE sp_IngresarAnexo (@IdAnexo int, @NombreAnexo varchar(50))
 
+as
+	if((@IdAnexo='') or (@NombreAnexo=''))
+		BEGIN
+			Print 'INGRESO DE DATOS INCOMPLETO'
+			return
+		END
+	else
+		BEGIN
+			If not exists (select IdAnexo from Anexos where IdAnexo = @IdAnexo)
+
+		BEGIN
+			Insert into Anexos(IdAnexo, NombreAnexo)
+
+			Values(@IdAnexo, @NombreAnexo)
+
+			Print 'DATOS INGRESADOS'
+
+		END
+	else
+		BEGIN
+			If exists (select IdAnexo from Anexos where IdAnexo = @IdAnexo)
+			BEGIN
+				Print 'DATOS DUPLICADOS'
+			END
+		END
+	END
+GO
 --Modificacion de datos
+Create PROCEDURE sp_ModificarAnexo (@IdAnexo int, @NombreAnexo varchar(50))
 
+as
+	if((@IdAnexo='') or (@NombreAnexo=''))
+		BEGIN
+			Print 'INGRESO DE DATOS INCOMPLETO'
+			return
+		END
+	else
+		BEGIN
+			If not exists (select IdAnexo from Anexos where IdAnexo = @IdAnexo)
+
+			BEGIN
+				PRINT 'DATOS NO REGISTRADOS'
+			END
+		else
+			
+			BEGIN
+				if exists (select IdAnexo from Anexos where IdAnexo = @IdAnexo)
+				BEGIN
+					update Anexos set NombreAnexo = @NombreAnexo
+					
+					Print 'ACTUALIZACION COMPLETA'
+				END
+			END
+		END
+GO
 --Borrar datos
+Create PROCEDURE sp_BorrarAnexo (@IdAnexo int)
 
+as
+	if((@IdAnexo=''))
+		BEGIN
+			Print 'INGRESO DE DATOS INCOMPLETO'
+			return
+		END
+	else
+		BEGIN
+
+			if not exists (select IdAnexo from Anexos where IdAnexo = @IdAnexo)
+				
+				BEGIN
+					Print 'DATOS NO REGISTRADOS'	
+		END
+	else
+		BEGIN
+			If exists (select IdAnexo from Anexos where IdAnexo = @IdAnexo)
+			
+			BEGIN
+				delete from Anexos where IdAnexo = @IdAnexo
+				Print 'DATOS ELIMINADOS'
+			END
+		END
+	END
+GO
 --Buscar datos
+Create PROCEDURE sp_BuscarAnexo (@IdAnexo int)
+as
+	if((@IdAnexo = null))
+	
+		BEGIN
+			Print 'DATOS NO INGRESADOS'
+		END
 
---------------------------------------------------------------------
+	else
+		
+		BEGIN
+			If exists (select IdAnexo from Anexos where IdAnexo = @IdAnexo)
+				BEGIN
+					Select	IdAnexo, NombreAnexo From Anexos where IdAnexo= @IdAnexo
+				END
+		END	
+GO
+-----------------------*---------------------------------------------
 --Tabla Comentarios
 --Procedimiento almacenado
 
